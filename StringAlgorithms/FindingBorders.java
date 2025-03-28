9e920901675d34c100277a2d325251d8a5d20d1d
Prem-Duvvapu <prem.duvvapu@gmail.com>
2025-03-28 19:03:45 +0000

    tle fix

--- StringAlgorithms/FindingBorders.java
+++ StringAlgorithms/FindingBorders.java
@@ -14,17 +14,18 @@ public class FindingBorders {
 class Solution {
     public void solve(String s) {
         int n=s.length();
+        StringBuilder res=new StringBuilder();
 
         long mod=(long)1e9+7;
         long p1=31;
         long p2=37;
         
-        long[] power1=new long[n+1];
-        long[] power2=new long[n+1];
+        long[] power1=new long[n];
+        long[] power2=new long[n];
 
-        power1[0]=power2[1]=1;
+        power1[0]=power2[0]=1;
 
-        for (int i=1;i<n+1;i++) {
+        for (int i=1;i<n;i++) {
             power1[i]=(power1[i-1]*p1)%mod;
             power2[i]=(power2[i-1]*p2)%mod;
         }
@@ -34,18 +35,20 @@ class Solution {
         long suffixHash1=0;
         long suffixHash2=0;
 
-        for (int i=0;i<n;i++) {
+        for (int i=0;i<n-1;i++) {
             int leftVal=s.charAt(i)-'a'+1;
             int rightVal=s.charAt(n-1-i)-'a'+1;
             
             prefixHash1=(prefixHash1+leftVal*power1[i])%mod;
-            suffixHash1=(suffixHash1*power1[i]+rightVal)%mod;
+            suffixHash1=(suffixHash1*p1+rightVal)%mod;
 
             prefixHash2=(prefixHash2+leftVal*power2[i])%mod;
-            suffixHash2=(suffixHash2*power2[i]+rightVal)%mod;
+            suffixHash2=(suffixHash2*p2+rightVal)%mod;
 
-            if ((prefixHash1==suffixHash1) && (suffixHash1==suffixHash2))
-                System.out.print((i+1)+" ");
+            if ((prefixHash1==suffixHash1) && (prefixHash2==suffixHash2))
+                res.append(i+1).append(" ");
         }
+
+        System.out.println(res);
     }
 }
